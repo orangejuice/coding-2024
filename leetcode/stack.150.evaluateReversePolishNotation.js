2e94073e1898930141bd32ef4e552a8a2f5f704b
orangejuice <cherrymeteor@outlook.com>
2024-04-19 18:25:14 +0100

    q.150

---
+++ leetcode/stack.150.evaluateReversePolishNotation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN2 = function (tokens) {
+  const stack = []
+
+  for (const ch of tokens) {
+    if (/\d+/.test(ch)) stack.push(ch)
+    else switch (ch) {
+      case '+': stack.push(+stack.pop() + +stack.pop()); break
+      case '-': stack.push(-stack.pop() + +stack.pop()); break
+      case '*': stack.push(+stack.pop() * +stack.pop()); break
+      case '/': stack.push(Math.trunc(1 / +stack.pop() * +stack.pop()))
+    }
+  }
+  return stack[0]
+};
+
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+  const stack = []
+  for (const ch of tokens) {
+    switch (ch) {
+      case '+': stack.push(+stack.pop() + +stack.pop()); break
+      case '-': stack.push(-stack.pop() + +stack.pop()); break
+      case '*': stack.push(+stack.pop() * +stack.pop()); break
+      case '/': stack.push(Math.trunc(1 / +stack.pop() * +stack.pop())); break
+      default: stack.push(ch)
+    }
+  }
+  return stack[0]
+};
