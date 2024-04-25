448b1a4b8d9e99c1ea3f2110a8c6b8c89c992675
orangejuice <cherrymeteor@outlook.com>
2024-04-25 17:42:53 +0100

    q.45

--- leetcode/array.45.jumpGameII.js
+++ leetcode/array.45.jumpGameII.js
@@ -1,12 +1,13 @@
 /**
+ * This version of code counted the first jump twice and did not count the last jump, to simplify the code
  * @param {number[]} nums
  * @return {number}
  */
 var jump = function (nums) {
-  let reach = nums[0], nextReach = 0, jump = 0
+  let reach = 0, nextReach = 0, jump = 0
   for (let i = 0; i < nums.length - 1; i++) {
     nextReach = Math.max(nextReach, i + nums[i])
     if (i == reach) { reach = nextReach; jump++ }
   }
-  return nums.length > 1 ? jump + 1 : jump
+  return jump
 }
