5fe13ce7cbde896727f448cffce1072e7456e96e
orangejuice <cherrymeteor@outlook.com>
2024-04-26 21:10:34 +0100

    q.134

--- leetcode/array.134.gasStation.js
+++ leetcode/array.134.gasStation.js
@@ -4,19 +4,16 @@
  * @return {number}
  */
 var canCompleteCircuit = function (gas, cost) {
-  let tank = 0, start = 0
-  for (let i = 0; i < gas.length + start; i++) {
+  let start = 0, tank = 0
+  for (let i = 0; i - start < gas.length; i++) {
     let cur = i % gas.length
-
     tank += gas[cur]
-    if (tank < cost[cur]) {
+    if (cost[cur] <= tank) tank -= cost[cur]
+    else {
       if (i >= gas.length - 1) return -1
-      start = (cur + 1) % gas.length
+      start = (i + 1) % gas.length
       tank = 0
-    } else {
-      tank -= cost[cur]
     }
   }
-
   return start
-}
+};
