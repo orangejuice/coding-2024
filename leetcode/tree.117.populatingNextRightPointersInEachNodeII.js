39f7a7f487d624ce3674ea36ba1b2f1266f67291
orangejuice <cherrymeteor@outlook.com>
2024-05-06 19:34:39 +0100

    117

---
+++ leetcode/tree.117.populatingNextRightPointersInEachNodeII.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function (root) {
+  if (!root) return root
+  let queue = [root]
+  while (queue.length > 0) {
+    const row = []
+    while (queue.length > 0) {
+      const cur = queue.shift()
+      if (cur.left) {
+        if (row.length > 0) row[row.length - 1].next = cur.left
+        row.push(cur.left)
+      }
+      if (cur.right) {
+        if (row.length > 0) row[row.length - 1].next = cur.right
+        row.push(cur.right)
+      }
+    }
+    queue = row
+  }
+  return root
+};
