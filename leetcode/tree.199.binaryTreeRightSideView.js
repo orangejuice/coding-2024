7e99829799068d7dfcc8da0a1f403ed77ae833d0
orangejuice <cherrymeteor@outlook.com>
2024-06-07 09:03:42 +0100

    199

---
+++ leetcode/tree.199.binaryTreeRightSideView.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+  if(!root) return []
+  let queue = [root], row = [], result = []
+
+  while (queue.length > 0) {
+    const cur = queue.shift()
+    if (cur.left !== null) row.push(cur.left)
+    if (cur.right !== null) row.push(cur.right)
+
+    if (queue.length == 0) {
+      result.push(cur.val)
+      queue.push(...row)
+      row = []
+    }
+  }
+  return result
+};
