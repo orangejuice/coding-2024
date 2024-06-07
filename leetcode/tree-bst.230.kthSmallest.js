0589357f7486976dbd05bef10a898ceab0f9fd07
orangejuice <cherrymeteor@outlook.com>
2024-06-07 14:29:20 +0100

    230

---
+++ leetcode/tree-bst.230.kthSmallest.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function (root, k) {
+  let i = 1, result
+
+  const iterate = (node) => {
+    if (!node) return
+    iterate(node.left)
+    if (i++ == k) result = node.val
+    else iterate(node.right)
+  }
+  iterate(root)
+  return result
+};
