e15c0fd3650ff9cb7b7cf2293d8f504ac3f477d5
orangejuice <cherrymeteor@outlook.com>
2024-06-07 09:48:22 +0100

    637

---
+++ leetcode/tree.637.averageOfLevelsInBinaryTree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function (root) {
+  if (!root) return []
+  let queue = [root], row = [], result = [root.val]
+  while (queue.length > 0) {
+    const cur = queue.shift()
+    if (cur.left != null) row.push(cur.left)
+    if (cur.right != null) row.push(cur.right)
+
+    if (queue.length == 0) {
+      if (row.length) result.push(row.reduce((acc, cur) => acc + cur.val, 0) / row.length)
+      queue = row
+      row = []
+    }
+  }
+  return result
+};
