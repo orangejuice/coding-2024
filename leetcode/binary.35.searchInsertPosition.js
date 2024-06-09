03be2061aa12d41ceae6a6e95f1c3f4356bfddb3
orangejuice <cherrymeteor@outlook.com>
2024-06-09 17:54:25 +0100

    35

---
+++ leetcode/binary.35.searchInsertPosition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+
+  const search = (start, end) => {
+    if (start > end) return start
+    const mid = Math.floor((start + end) / 2)
+
+    if (nums[mid] == target) return mid
+    else if (nums[mid] < target) return search(mid + 1, end)
+    else return search(start, mid - 1)
+  }
+
+  return search(0, nums.length - 1)
+};
