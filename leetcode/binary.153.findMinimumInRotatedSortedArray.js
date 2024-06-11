82122c7b95cb6266500c17055ec00f8d48e65a37
orangejuice <cherrymeteor@outlook.com>
2024-06-11 13:07:17 +0100

    153.v2

--- leetcode/binary.153.findMinimumInRotatedSortedArray.js
+++ leetcode/binary.153.findMinimumInRotatedSortedArray.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var findMin = function (nums) {
-  let min = Infinity
   const find = (start, end) => {
-    if (start > end) return
-    if (nums[start] < nums[end]) {
-      min = Math.min(min, nums[start])
-    } else {
-      const mid = Math.floor((start + end) / 2)
-      min = Math.min(min, nums[mid], nums[end])
-      find(start, mid - 1)
-      find(mid + 1, end)
-    }
+    if (start >= end) return start
+    const mid = Math.floor((start + end) / 2)
+    if (nums[mid] < nums[end]) return find(start, mid)
+    if (nums[mid] > nums[end]) return find(mid + 1, end)
   }
-  find(0, nums.length - 1)
-  return min
-}
+  return nums[find(0, nums.length - 1)]
+};
+
+/*
+*   start >= end              includes start == end
+*   nums[mid] < nums[end]     minimum locates in [start, mid]
+*   nums[mid] > nums[end]     minimum locates in [mid+1, end]
+*/
