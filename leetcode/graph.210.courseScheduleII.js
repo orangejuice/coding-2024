f5aa551334ece4aa80ded3a5c7c75a9668adc2ef
orangejuice <cherrymeteor@outlook.com>
2024-06-11 17:59:11 +0100

    210.v2

--- leetcode/graph.210.courseScheduleII.js
+++ leetcode/graph.210.courseScheduleII.js
@@ -4,19 +4,30 @@
  * @return {number[]}
  */
 var findOrder = function (numCourses, prerequisites) {
-  const dependsOn = Array.from({length: numCourses}, () => new Set())
-  prerequisites.forEach(([a, b]) => {
-    dependsOn[a].add(b)
-    dependsOn[b].forEach(course => dependsOn[a].add(course))
-  })
-  const result = []
+  const graph = Array.from({length: numCourses}, () => [])
+  prerequisites.forEach(([a, b]) => graph[a].push(b))
+  const result = new Set()
 
-  while (result.length < numCourses) {
-    const next = dependsOn.findIndex(courses => courses?.size == 0)
-    if (next == -1) return []
-    result.push(next)
-    dependsOn[next] = null
-    dependsOn.forEach((courses) => courses?.delete(next))
+  const dfs = (course, visited) => {
+    if (result.has(course)) return true
+    if (visited.has(course)) return false
+    visited.add(course)
+
+    for (const pre of graph[course]) {
+      if (!dfs(pre, visited)) return false
+    }
+    result.add(course)
+    return true
   }
-  return result
+
+  for (let i = 0; i < numCourses; i++) {
+    if (!dfs(i, new Set())) return []
+  }
+
+  return [...result]
 }
+
+/*
+ *     if (result.has(course)) return true       -> firstly, check if the course has already been studied
+ *     if (visited.has(course)) return false     -> if not studied, and visited has it already, that means a cycle
+ */
