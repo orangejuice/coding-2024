70dd70616b4fea070dad168714615916146b7830
orangejuice <cherrymeteor@outlook.com>
2024-06-13 16:11:56 +0100

    148

---
+++ leetcode/divide.148.sort-list.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function (head) {
+  if (!head || !head.next) return head
+
+  const getMid = () => {
+    let slow = head, fast = head.next
+    while (fast && fast.next) {
+      fast = fast.next.next
+      slow = slow.next
+    }
+    return slow
+  }
+
+  const merge = (left, right) => {
+    const dummy = new ListNode()
+    let cur = dummy
+    while (left && right) {
+      if (left.val > right.val) {
+        cur.next = right
+        right = right.next
+      } else {
+        cur.next = left
+        left = left.next
+      }
+      cur = cur.next
+    }
+    cur.next = left || right
+    return dummy.next
+  }
+
+  const mid = getMid(head), right = mid.next
+  mid.next = null
+  return merge(sortList(head), sortList(right))
+}
