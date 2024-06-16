495ae91617231df68a7e1af3c2d173474ad0b6d2
orangejuice <cherrymeteor@outlook.com>
2024-06-16 15:50:15 +0100

    88

--- leetcode/array.88.merge-sorted-array.js
+++ leetcode/array.88.merge-sorted-array.js
@@ -6,13 +6,11 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-  for (let i = m + n - 1; i >= 0; i--) {
-    if (nums1[m - 1] > nums2[n - 1] || n < 1) {
-      nums1[i] = nums1[m - 1]
-      m--
-    } else {
-      nums1[i] = nums2[n - 1]
-      n--
-    }
+  let i = m - 1, j = n - 1, cur = m + n - 1
+  while (i >= 0 || j >= 0) {
+    if (i < 0) nums1[cur--] = nums2[j--]
+    else if (j < 0) nums1[cur--] = nums1[i--]
+    else if (nums1[i] > nums2[j]) nums1[cur--] = nums1[i--]
+    else nums1[cur--] = nums2[j--]
   }
-}
+};
