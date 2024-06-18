96975b050c98282a28579a06e4fdd1a5850ad6a6
orangejuice <cherrymeteor@outlook.com>
2024-06-18 16:55:21 +0100

    26

--- leetcode/array.26.remove-duplicates-from-sorted-array.js
+++ leetcode/array.26.remove-duplicates-from-sorted-array.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let validIdx = 0
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] != nums[validIdx]) nums[++validIdx] = nums[i]
+  let fast = 1, slow = 1
+  while (fast < nums.length) {
+    if (nums[fast] != nums[fast - 1]) nums[slow++] = nums[fast]
+    fast++
   }
-  return validIdx + 1
-}
+  return slow
+};
