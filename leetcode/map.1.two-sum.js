985b4af27f947293d585183f2ca25ca5e69cd402
orangejuice <cherrymeteor@outlook.com>
2024-06-18 23:07:21 +0100

    1.v1

--- leetcode/map.1.two-sum.js
+++ leetcode/map.1.two-sum.js
@@ -4,18 +4,22 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let map = new Map()
+  let i = 0, map = new Map()
   for (let i = 0; i < nums.length; i++) {
-    if (map.has(nums[i])) map.get(nums[i]).push(i)
-    else map.set(nums[i], [i])
+    const num = nums[i]
+    if (map.has(num)) map.get(num).push(i)
+    else map.set(num, [i])
   }
 
-  for (let i = 0; i < nums.length; i++) {
-    if (!map.has(target - nums[i])) continue
-    if (nums[i] == target - nums[i]) {
-      if (map.get(nums[i]).length > 1) return map.get(nums[i])
-    } else {
-      return [i, map.get(target - nums[i])[0]]
+  for (let num of map.keys()) {
+    if (map.has(target - num)) {
+      if (num == target - num) {
+        if (map.get(num).length > 1) {
+          return map.get(num).slice(0, 2)
+        }
+      } else {
+        return [map.get(num)[0], map.get(target - num)[0]]
+      }
     }
   }
-}
+};
