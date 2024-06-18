e1efc8e321bf6d83fa9637f790e19603ce791f2d
orangejuice <cherrymeteor@outlook.com>
2024-06-18 18:47:57 +0100

    80

--- leetcode/array.80.remove-duplicates-ii.js
+++ leetcode/array.80.remove-duplicates-ii.js
@@ -1,12 +1,20 @@
 /**
- * @param {number[]} nums sorted ascending nums
+ * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  if (nums.length < 2) return nums.length
-  let validI = 1
-  for (let i = 2; i < nums.length; i++) {
-    if (nums[i] != nums[validI - 1]) nums[++validI] = nums[i]
+  if (nums.length < 3) return nums.length
+  let fast = 1, slow = 0, cur = nums[0], count = 1
+
+  while (fast <= nums.length) {
+    if (nums[fast] == cur && fast < nums.length) {
+      if (count < 2) count += 1
+    } else {
+      while (count-- > 0) nums[slow++] = cur
+      cur = nums[fast]
+      count = 1
+    }
+    fast++
   }
-  return validI + 1
-}
+  return slow
+};
