fa71b786581317747cfb8f914a7028d491e333e5
orangejuice <cherrymeteor@outlook.com>
2024-06-25 15:35:45 +0100

    121

--- leetcode/array.121.best-time-to-buy-and-sell-stock.js
+++ leetcode/array.121.best-time-to-buy-and-sell-stock.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let low = prices[0], profit = 0
-  for (let i = 0; i < prices.length; i++) {
-    low = Math.min(low, prices[i])
-    profit = Math.max(profit, prices[i] - low)
+  let low = prices[0], maxProfit = 0
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < low) low = prices[i]
+    else maxProfit = Math.max(maxProfit, prices[i] - low)
   }
-  return profit
-}
+  return maxProfit
+};
