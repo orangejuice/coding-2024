ed6ad48ec4e93051e89a1ea2147b9382f9cee072
orangejuice <cherrymeteor@outlook.com>
2024-06-26 11:48:58 +0100

    49

--- leetcode/49.group-anagrams.js
+++ leetcode/49.group-anagrams.js
@@ -3,30 +3,13 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  let resultMap = new Map()
-  for (let word of strs) {
-    let map = new Map()
-    for (let ch of word) map.set(ch, (map.get(ch) ?? 0) + 1)
-    let key = Array.from(map.entries(), entry => entry.join("")).sort().join("")
-    if (resultMap.has(key)) resultMap.get(key).push(word)
-    else resultMap.set(key, [word])
+  const map = new Map()
+
+  for(let str of strs) {
+    const sorted = [...str].sort().join('')
+    if(map.has(sorted)) map.get(sorted).push(str)
+    else map.set(sorted, [str])
   }
 
-  return [...resultMap.values()]
-}
-
-
-/**
- * ok, no need to create a map
- * @param {string[]} strs
- * @return {string[][]}
- */
-var groupAnagrams = function (strs) {
-  let resultMap = new Map()
-  for (let word of strs) {
-    let key = Array.from(word).sort().join("")
-    if (resultMap.has(key)) resultMap.get(key).push(word)
-    else resultMap.set(key, [word])
-  }
-  return [...resultMap.values()]
-}
+  return [...map.values()]
+};
