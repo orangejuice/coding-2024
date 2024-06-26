8f11adba01debd956ff1fe832a4999fec4efef10
orangejuice <cherrymeteor@outlook.com>
2024-06-26 17:02:35 +0100

    238.space o(n)

--- leetcode/array.238.product-of-array-without-self.js
+++ leetcode/array.238.product-of-array-without-self.js
@@ -3,13 +3,14 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  let result = new Array(nums.length).fill(1)
-  for (let i = 1; i < result.length; i++) {
-    result[i] = result[i - 1] * nums[i - 1]
-  }
-  for (let i = result.length - 2, multiply = 1; i >= 0; i--) {
-    multiply *= nums[i + 1]
-    result[i] *= multiply
-  }
-  return result
-}
+  const leftProduct = Array(nums.length)
+  const rightProduct = Array(nums.length)
+  leftProduct[0] = 1
+  rightProduct[nums.length - 1] = 1
+  for (let i = 1; i < nums.length; i++)
+    leftProduct[i] = leftProduct[i - 1] * nums[i - 1]
+  for (let i = nums.length - 2; i >= 0; i--)
+    rightProduct[i] = rightProduct[i + 1] * nums[i + 1]
+
+  return nums.map((_, i) => leftProduct[i] * rightProduct[i])
+};
